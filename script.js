fbf361bec68f48e69d5a3bc5f0d0e31c7cec00d5
Theo <46166826+cheftheo@users.noreply.github.com>
2022-09-12 15:02:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	// $("#attackers .mafiaName").css("color","red");
+	// $("#defenders .mafiaName").css("color","blue");
+
+
+	// animate .radiusColor on appears to red
+	$(".radiusColor").css("background-color","#c09cff");	
+	$(".radiusColor").animate({opacity: "100%"}, 4500);
+});
